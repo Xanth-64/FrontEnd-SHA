e53a3158d12164fc4e2bfbc95698e9c51eb2d938
Andrés Betancourt <30258079@correo.unimet.edu.ve>
2022-07-12 02:23:50 +0000

    Adds WelcomeCard Component to Show SignUp / Login

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
-
+import { Grid } from '@mantine/core';
+import WelcomeCard from '../../components/organisms/WelcomeCard';
 const Home: NextPage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <Grid gutter={20}>
+      <Grid.Col span={12} xs={6} offsetXs={3}>
+        <WelcomeCard />
+      </Grid.Col>
+    </Grid>
+  );
 };
 
 export default Home;
